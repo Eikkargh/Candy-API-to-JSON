26b783c4688cf372f7bc2ac41dcd0801ee4f9db0
Eikkargh <87714048+Eikkargh@users.noreply.github.com>
2023-01-19 21:28:09 +0000

    Add files via upload

---
+++ candy.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+import requests
+import json
+import time
+
+url = 'http://192.168.1.24/http-read.json?encrypted=1'
+key = 'fhaihbiklhjdlmga'
+request_timeout = 10
+tries = 0
+retries = 5
+candy = {}
+candyOff = {'WiFiStatus':'1', 'Err':None, 'MachMd':None, 'Pr':None, 'PrPh':None, 'Temp':None, 'SpinSp':None, 'RemTime':'0', 'DryT':'0', 'DelVal':0, 'TotalTime':'0'}
+
+#extract data
+def fetchHex(xurl, xrequest_timeout):
+    try:
+        candyhex = requests.get(xurl, timeout=xrequest_timeout).text
+        return candyhex
+    except:
+        return None
+
+# convert data to readable text
+def convText():
+    hexText = fetchHex(url, request_timeout)
+    if hexText == None:
+        return None
+    bytes_object = bytes.fromhex(hexText)
+    coded = bytes_object.decode("ASCII")
+    return coded
+
+#decode data
+def decode(xkey):
+    xored = str()
+    codedText = convText()
+    if codedText == None:
+        return None
+    repeated_key = (xkey)*((len(codedText) // len(xkey)) + 1)
+    for x in range(len(codedText)):
+        xored += chr(ord(codedText[x]) ^ ord(repeated_key[x]))
+    return xored
+
+# strip and print data
+while tries < retries:
+    decoded = decode(key)
+    if decoded != None:
+        candyJson = json.dumps(candyOff, indent = 4)
+        decodedDict = json.loads(decoded)
+        candyData = decodedDict.get('statusLavatrice')
+        for k, v in candyData.items():
+            if k[0:3] != "Opt" and k[0:3] != "Rec" and k[0:3] != "Ste" and k[0:3] != "SLe" and k[0:3] != "Che":
+                if k == 'DelVal' and candyData[k] == '255':
+                    candy['DelVal'] = '0'
+                else:
+                    candy[k] = candyData[k]
+        TotalTime = int(candy['DelVal']) * 60 + int(candy['RemTime'])
+        candy['TotalTime'] = str(TotalTime)
+        candyJson = json.dumps(candy, indent = 4)
+        break
+    tries += 1
+    time.sleep(2)
+if tries == retries:
+    candyJson = json.dumps(candyOff, indent = 4)
+print(candyJson)
